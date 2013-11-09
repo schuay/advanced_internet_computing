e306a7153afb417fa5e1e3b1c394701971a73468
Jakob Gruber <jakob.gruber@gmail.com>
2013-11-09 13:49:49 +0100

    Convert important date fields to datetime objects

--- src/tweetstore.py
+++ src/tweetstore.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from pymongo import MongoClient
 
+_CREATED_AT = "created_at"
+
 """A (somewhat) generic store for tweets which can be queried by
 key words and time ranges, and filled with a list of tweets."""
 class TweetStore:
@@ -21,6 +23,18 @@ class TweetStore:
         return c.find({
             "text": {"$regex": ".*Call.*"}})
 
+    """Twython gives us date fields as strings. This function converts date fields we care
+    about (such as "created_at") into proper datetime objects."""
+    def _str_to_date(self, tweet):
+        if type(tweet[_CREATED_AT]) is datetime:
+            return tweet
+        # strptime does not always accept %z for the timezone in 2.7, so we have
+        # to handle it manually. The initial format is: Sun Oct 20 19:48:26 +0000 2013
+        datestr = tweet[_CREATED_AT]
+        dt = datetime.strptime(datestr[0:20] + datestr[26:], "%a %b %d %H:%M:%S %Y")
+        tweet[_CREATED_AT] = dt
+        return tweet
+
     """Stores the specified tweets into the database."""
     def put(self, tweets):
         self._collection.insert(tweets)
@@ -33,5 +47,9 @@ if __name__ == "__main__":
     ts = TweetStore("test_database")
     for t in ts.get([], datetime(2013, 1, 1), datetime(2999, 1, 1)):
         print t["text"]
-        print type(t["created_at"])
+        print t[_CREATED_AT]
+        print type(t[_CREATED_AT])
+        ts._str_to_date(t)
+        print t[_CREATED_AT]
+        print type(t[_CREATED_AT])
 
