d375e36c7cb5344f1a0ead85eabf6e227422043a
Jakob Gruber <jakob.gruber@gmail.com>
2013-11-09 12:51:19 +0100

    Documentation and a test date range

--- src/tweetstore.py
+++ src/tweetstore.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pymongo import MongoClient
 
 """A (somewhat) generic store for tweets which can be queried by
@@ -12,10 +13,13 @@ class TweetStore:
         self._collection = self._client[dbname].test_collection
 
     """Retrieves a list of tweets in twython's format from the database.
-    Tweets are filtered by the specified keywords and time range."""
-    def get(self, keywords, timerange):
+    Tweets are filtered by the specified keywords and time range.
+    keywords is a list of strings.
+    start and end are datetime objects."""
+    def get(self, keywords, start, end):
         c = self._collection
-        return c.find({"text": {"$regex": ".*Call.*"}})
+        return c.find({
+            "text": {"$regex": ".*Call.*"}})
 
     """Stores the specified tweets into the database."""
     def put(self, tweets):
@@ -27,6 +31,7 @@ class TweetStore:
 
 if __name__ == "__main__":
     ts = TweetStore("test_database")
-    for t in ts.get([], []):
+    for t in ts.get([], datetime(2013, 1, 1), datetime(2999, 1, 1)):
         print t["text"]
+        print type(t["created_at"])
 
