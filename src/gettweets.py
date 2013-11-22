cc868d02464f6f7c75c8585ee5e87e7c04dad0c4
Jakob Gruber <jakob.gruber@gmail.com>
2013-11-22 16:08:19 +0100

    Remove threading, pt. 2
    This also fixes an issue in that streaming tweets were not filtered
    by keywords if both location and track parameters were specified (apparently
    it OR's them).

--- src/gettweets.py
+++ src/gettweets.py
@@ -19,11 +19,11 @@ CREDENTIALS_FILE = 'credentials.txt'
 WORLD = '-180,-90,180,90'
 TIMEOUT = 900
 
-manager = None
+fetcher = None
 
 def signal_handler(signal, frame):
-    if manager is not None:
-        manager.stop()
+    if fetcher is not None:
+        fetcher.stop()
 
 class MyStreamer(TwythonStreamer):
     """In addition to initializing the TwythonStreamer class, sets our tweet store
@@ -36,8 +36,15 @@ class MyStreamer(TwythonStreamer):
         self._search_to = search_to
 
     def on_success(self, data):
-        self._buffer.append(data)
-        self._parent.updateStreamProgress()
+        t = tweet.to_date(data)
+
+        if t[tweet.CREATED_AT] > self._search_to:
+            self.flush()
+            self.disconnect()
+            return
+
+        self._buffer.append(t)
+        self._parent.update()
 
         if len(self._buffer) > CHUNK_SIZE:
             self.flush()
@@ -46,48 +53,12 @@ class MyStreamer(TwythonStreamer):
         print status_code
         self.disconnect()
 
-    def check_tweets(self, tweets):
-        map(tweet.to_date, tweets)
-        t = [t for t in tweets if t[tweet.CREATED_AT] <= self._search_to]
-        if len(t) > 0:
-            self._store.put(t)
-        else:
-            self.disconnect()
-
     def flush(self):
-        self.check_tweets(self._buffer)
+        self._store.put(self._buffer)
         del self._buffer[:]
-
-class StreamThread:
-    def __init__(self, credentials, search_kw, search_to, parent):
-        self._parent = parent
-        self._credentials = credentials
-        self._search_kw = search_kw
-        self._search_to = search_to
-        self._stream = None
-    def exit(self):
-        if self._stream is not None:
-            self._stream.flush()
-            self._stream.disconnect()
-    def run(self):
-        store = TweetStore("tweets")
-        try:
-            self._stream = MyStreamer(self._credentials['APP_KEY'],
-                                self._credentials['APP_SECRET'],
-                                self._credentials['TOKEN_KEY'],
-                                self._credentials['TOKEN_SECRET'],
-                                store,
-                                self._parent,
-                                self._search_to)
-            self._stream.statuses.filter(locations = WORLD,
-                                   language = 'en',
-                                   track = self._search_kw)
-        finally:
-            store.close()
 
 class TweetFetcher():
     def __init__(self, credentials, search_kw, search_from, search_to):
-        self._buffer = []
         self._credentials = credentials
         self._loop = True
         self._retrieved = 0
@@ -110,6 +81,10 @@ class TweetFetcher():
         try:
             self._store = TweetStore("tweets")
             self._fetch_tweets()
+
+            if not self._loop:
+                return
+
             self._fetch_stream_tweets()
         finally:
             if self._store is not None:
@@ -152,20 +127,30 @@ class TweetFetcher():
                 break
 
     def _fetch_stream_tweets(self):
-        pass
+        self._stream = MyStreamer(self._credentials['APP_KEY'],
+                            self._credentials['APP_SECRET'],
+                            self._credentials['TOKEN_KEY'],
+                            self._credentials['TOKEN_SECRET'],
+                            self._store,
+                            self,
+                            self._search_to)
+        self._stream.statuses.filter(language = 'en',
+                                     track = self._search_kw)
 
-    def update(self, count):
+    def update(self, count = 1):
         self._retrieved += count
         self._progressbar.update(self._retrieved)
 
     def stop(self):
-        self._store.put(self._buffer)
         self._loop = False
+        if self._stream is not None:
+            self._stream.flush()
+            self._stream.disconnect()
 
 if __name__ == '__main__':
     search_from = datetime.utcnow()
-    search_to = datetime.utcnow()
-    search_kw = "The Beatles"
+    search_to = datetime.utcnow() + timedelta(days = 1)
+    search_kw = "The Beatles" # TODO: Allow multiple keywords
 
     opts, args = getopt.getopt(sys.argv[1:], "q:f:t:h")
     for o, a in opts:
@@ -178,6 +163,8 @@ if __name__ == '__main__':
         else:
             print("USAGE: %s [-q query] [-f from_date] [-t to_date]" % sys.argv[0])
             sys.exit(1)
+
+    print "Retrieving matches for '%s' from %s to %s" % (search_kw, str(search_from), str(search_to))
 
     signal.signal(signal.SIGINT, signal_handler)
 
