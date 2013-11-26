1e1999457b6ac317158a98afd61dbaf4ffa29224
guItDuvHas <guItDuvHas@example.com>
2013-11-26 21:19:23 +0100

    Fix missing import for ConnectionError

--- src/gettweets.py
+++ src/gettweets.py
@@ -7,6 +7,7 @@ import sys
 import time
 import tweet
 
+from requests import ConnectionError
 from datetime import datetime, timedelta
 from progressbar import ProgressBar, AnimatedMarker, Timer, Counter, UnknownLength
 import tweetstore
