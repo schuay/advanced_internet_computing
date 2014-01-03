a7e9118be57ef68c439440554954910324569613
guItDuvHas <guItDuvHas@example.com>
2014-01-03 10:44:50 +0100

    Add Aggregator weighting tweets by number of retweets

--- src/aggregator.py
+++ src/aggregator.py
@@ -25,7 +25,7 @@ class AggregatorI:
         with open(filename, 'wb') as f:
             pickle.dump(self, f)
 
-class MeanAggregator:
+class MeanAggregator(AggregatorI):
     def __init__(self):
         self.__last_id = 0
         self.__num = 0.0
@@ -40,6 +40,32 @@ class MeanAggregator:
         if (self.__num == 0.0):
             return None
         return self.__sentiment_ctr / self.__num
+
+    def get_last_id(self):
+        return self.__last_id
+
+    def get_num(self):
+        return self.__num
+
+class RetweetWeightedAggregator(AggregatorI):
+    def __init__(self):
+        self.__last_id = 0
+        self.__num = 0
+        self.__weighted_num = 0.0
+        self.__sentiment_ctr = 0.0
+
+    def add(self, t, sentiment):
+        weight = t[tweet.RETWEET_COUNT] + 1
+
+        self.__sentiment_ctr += weight * sentiment
+        self.__weighted_num += weight
+        self.__num += 1
+        self.__last_id = t[tweet.ID]
+
+    def get_sentiment(self):
+        if (self.__weighted_num == 0.0):
+            return None
+        return self.__sentiment_ctr / self.__weighted_num
 
     def get_last_id(self):
         return self.__last_id
